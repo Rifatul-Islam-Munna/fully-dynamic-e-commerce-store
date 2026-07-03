72c09dbae7165c3ac3cd3f7805b2c6e392fd10cf
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-04 01:29:47 +0600

    feat(seo): enable SEO settings controller

--- backend/src/web-settings/web-settings.module.ts
+++ backend/src/web-settings/web-settings.module.ts
@@ -2,21 +2,23 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WebSettingsService } from './web-settings.service';
 import { WebSettingsController } from './web-settings.controller';
+import { SeoSettingsController } from './seo-settings.controller';
+import { SeoSettingsService } from './seo-settings.service';
 import { FooterSetting } from './entities/footer-setting.entity';
 import { NavbarSetting } from './entities/navbar-setting.entity';
 import { HomeSetting } from './entities/home-setting.entity';
 import { SiteSetting } from './entities/site-setting.entity';
+import { SeoSetting } from './entities/seo-setting.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      NavbarSetting,
-      FooterSetting,
-      SiteSetting,
-      HomeSetting,
-    ]),
-  ],
-  controllers: [WebSettingsController],
-  providers: [WebSettingsService],
+  imports: [TypeOrmModule.forFeature([
+    NavbarSetting,
+    FooterSetting,
+    SiteSetting,
+    HomeSetting,
+    SeoSetting,
+  ])],
+  controllers: [WebSettingsController, SeoSettingsController],
+  providers: [WebSettingsService, SeoSettingsService],
 })
 export class WebSettingsModule {}
