8b57a2f839ccbc147bce47b5d59df22739ed030b
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-06 14:49:02 +0600

    refactor: expose modular homepage designs

---
+++ frontend/src/lib/home-section-variants.ts
@@ -0,0 +1,5 @@
+export * from "./home-section-variant-data/catalog";
+export * from "./home-section-variant-data/types";
+export * from "./home-section-variant-data/exports";
+export * from "./home-section-variant-data/normalize-export";
+export * from "./home-section-variant-data/meta-export";
